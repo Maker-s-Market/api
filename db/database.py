4d849831ca7669c3094e7f131773b0a9b23c2b3f
Mariana Andrade <marianaandrade@ua.pt>
2023-10-16 14:15:27 +0100

    basic structure

---
+++ db/database.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("MYSQL_URL", "mysql+pymysql://makers:makers@localhost/makers")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
