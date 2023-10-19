c7668fc92c26578cad3f52a61dc2d9bcbd064c5b
bruna <brunams21@ua.pt>
2023-10-19 14:19:32 +0100

    product testing

--- tests/routers/test_product.py
+++ tests/routers/test_product.py
@@ -45,12 +45,20 @@ class TestProduct:
             }
         )
 
-        assert response.status_code == 201
+        assert response.status_code == 201, response.text
+        
+        data = response.json()
+        assert data["name"] == "product1"
+        assert data["description"] == "product1's description"
+        assert data["price"] == 12.5
+        assert data["stockable"] == True
+        assert data["stock"] == 2
+        assert data["categories"] == []
 
     def test_put_existing_product(self):
         client = TestClient(app)
         
-        response = client.post("/product",
+        client.post("/product",
             json = {
                 "name": "product1",
                 "description": "product1's description",
@@ -72,6 +80,59 @@ class TestProduct:
             }
         )
 
-        assert response.status_code == 400
+        assert response.status_code == 400, response.text
+        assert response.json() == {'detail' : 'Error creating product'}
+
+    def test_wrong_category(self):
+        client = TestClient(app)
+
+        response = client.post("/product",
+            json = {
+                "name": "product1",
+                "description": "product1's description",
+                "price": 12.5,
+                "stockable": True,
+                "stock": 2,
+                "categories": [{
+                    "id": "non existent id"
+                }]
+            }
+        )
+
+        assert response.status_code == 404
+
+    def test_get_product(self):
+
+        client = TestClient(app)
+
+        response = client.post("/product",
+            json = {
+                "name": "product1",
+                "description": "product1's description",
+                "price": 12.5,
+                "stockable": True,
+                "stock": 2,
+                "categories": []
+            }
+        )
+
+        data = response.json()
+        product_id = data["id"]
+
+        response = client.get("/product/" + str(product_id))        #ig this could go wrong
+
+        assert response.status_code == 200
+        
+        data = response.json()
+
+        assert data["id"] == str(product_id)
+        assert data["name"] == "product1"
+        assert data["description"] == "product1's description"
+        assert data["price"] == 12.5
+        assert data["stockable"] == True
+        assert data["stock"] == 2
+        assert data["categories"] == []
+
+        pass
 
 
