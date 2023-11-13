f84a65bd9007a42f7b6ab4d952ff12316d3182cc
Bruna Simões <brunams21@ua.pt>
2023-11-13 09:45:16 +0000

    small changes

--- routers/ratingSeller.py
+++ routers/ratingSeller.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from auth.auth import get_current_user, jwks
 from repositories.userRepo import get_user_by_id
 from schemas.ratingSeller import CreateRatingSeller, UpdateRatingSeller
-from repositories.ratingSellerRepo import rating_in_db
+from repositories.ratingSellerRepo import rating_in_db, create_rating as cr
 
 auth = JWTBearer(jwks)
 
@@ -17,6 +17,7 @@ router = APIRouter(tags=['Rating The Seller'])
 async def create_seller_rating(rating: CreateRatingSeller, db: Session = Depends(get_db), username: str = Depends(get_current_user)):
     """
     function that creates a rating for a certain seller
+    TODO: test if functional
     """
     seller = get_user_by_id(rating.seller_id, db=db)
     if not seller:
@@ -28,3 +29,7 @@ async def create_seller_rating(rating: CreateRatingSeller, db: Session = Depends
                             content={"detail": "A rating for this seller was already created, please edit it instead"})
     return JSONResponse(status_code=201,
                         content=jsonable_encoder(cr(rating=rating, db=db, username=username).to_dict()))
+
+@router.put("/rating-seller", dependencies=[Depends(auth)])
+async def put_seller_rating():
+    pass
