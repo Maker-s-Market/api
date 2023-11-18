d7a5c9e7819fe195e6b74e2acad155966e05e91f
Mariana Andrade <marianaandrade@ua.pt>
2023-11-18 03:12:50 +0000

    fix tests

--- auth/auth.py
+++ auth/auth.py
@@ -7,7 +7,6 @@ from starlette.status import HTTP_403_FORBIDDEN
 
 from auth.JWTBearer import JWKS, JWTBearer, JWTAuthorizationCredentials
 
-load_path = os.path.join(os.path.dirname(__file__), ".aws")
 load_dotenv(".aws")  # Automatically load environment variables from a '.env' file.
 
 AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
