b392c8682336dc1829632d42a817a0a35e5ec3bf
Mariana Andrade <marianaandrade@ua.pt>
2023-11-19 01:32:57 +0000

    MM-12 test Done

--- routers/ratingSeller.py
+++ routers/ratingSeller.py
@@ -38,21 +38,21 @@ async def create_seller_rating(rating: CreateRatingSeller, db: Session = Depends
 
 
 @router.put("/rating-seller", dependencies=[Depends(auth)])
-async def put_seller_rating(rating: UpdateRatingSeller, db: Session = Depends(get_db),
+async def put_seller_rating(upd_rating: UpdateRatingSeller, db: Session = Depends(get_db),
                             username: str = Depends(get_current_user)):
     """
     endpoint that updates a rating made to a seller
     edit seller rating
     """
-    seller = get_user_by_id(rating.id, db=db)
+    seller = get_user_by_id(upd_rating.id, db=db)
     if not seller:
         raise HTTPException(status_code=404, detail="Seller not found")
-    rating = get_rating_by_seller_id_and_user(rating.seller_id, db, username)
+    rating = get_rating_by_seller_id_and_user(upd_rating.id, db, username)
     if not rating:
         raise HTTPException(status_code=404, detail="Rating not found")
-    if rating.rating < 0 or rating.rating > 5:
+    if upd_rating.rating < 0 or upd_rating.rating > 5:
         raise HTTPException(status_code=403, detail="Rating should be between 0 and 5")
-    rating.update(db, rating_up=rating)
+    rating.update(db, rating_up=upd_rating)
     seller.update_avg(db, float(avg(seller_id=rating.seller_id, db=db)))
     return JSONResponse(status_code=200, content=jsonable_encoder(rating.to_dict()))
 
--- tests/routers/test_rating_seller_IT.py
+++ tests/routers/test_rating_seller_IT.py
@@ -6,7 +6,7 @@ from starlette.testclient import TestClient
 from main import app
 from models.ratingSeller import RatingSeller
 from models.user import User
-from schemas.ratingSeller import CreateRatingSeller
+from schemas.ratingSeller import CreateRatingSeller, UpdateRatingSeller
 from tests.test_sql_app import TestingSessionLocal
 from dotenv import load_dotenv
 
@@ -122,3 +122,50 @@ def test_create_rating_seller_rating_cannot_rate_yourself():
 
     assert response.status_code == 403
     assert response.json() == {"detail": "You can not rate yourself"}
+
+
+def test_put_seller_rating_sucess():
+    rating = UpdateRatingSeller(rating=3, id="06e0da01-57fd-4441-95be-1111111111112")
+
+    response = client.put("/rating-seller",
+                          json=rating.model_dump(),
+                          headers={"Authorization": f"Bearer {login_user_1()}"})
+
+    assert response.status_code == 200
+    assert response.json()["rating"] == 3
+    assert response.json()["seller_id"] == "06e0da01-57fd-4441-95be-1111111111112"
+    assert response.json()["user_id"] == "06e0da01-57fd-4441-95be-1111111111111"
+
+
+def test_put_seller_rating_seller_not_found():
+    rating = UpdateRatingSeller(rating=3, id="06e0da01-57fd-4441-95be-1111111111113")
+
+    response = client.put("/rating-seller",
+                          json=rating.model_dump(),
+                          headers={"Authorization": f"Bearer {login_user_1()}"})
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Seller not found"}
+
+
+def test_put_seller_rating_rating_not_found():
+    rating = UpdateRatingSeller(rating=3, id="06e0da01-57fd-4441-95be-1111111111112")
+
+    response = client.put("/rating-seller",
+                          json=rating.model_dump(),
+                          headers={"Authorization": f"Bearer {login_user_2()}"})
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Rating not found"}
+
+
+def test_put_seller_rating_rating_not_between_0_and_5():
+    rating = UpdateRatingSeller(rating=6, id="06e0da01-57fd-4441-95be-1111111111112")
+
+    response = client.put("/rating-seller",
+                          json=rating.model_dump(),
+                          headers={"Authorization": f"Bearer {login_user_1()}"})
+
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Rating should be between 0 and 5"}
+
