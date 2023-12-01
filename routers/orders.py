f2126f2e313572936717b0fa74e15a45b0fad9f5
Mariana Andrade <marianaandrade@ua.pt>
2023-12-01 18:34:41 +0000

    MM-34 order history done

--- routers/orders.py
+++ routers/orders.py
@@ -8,7 +8,7 @@ from auth.auth import jwks, get_current_user
 from auth.JWTBearer import JWTBearer
 from db.database import get_db
 from models.orders.order_item import save_order_item
-from repositories.orderRepo import save_order
+from repositories.orderRepo import save_order, get_orders_by_user_id
 from repositories.productRepo import get_product_by_id
 from repositories.userRepo import get_user
 from schemas.order import CreateOrder
@@ -45,3 +45,10 @@ async def get_orders(products: List[CreateOrderItem], db: Session = Depends(get_
         save_order_item(item, order_db.id, db)
 
     return JSONResponse(status_code=201, content=jsonable_encoder(order_db.to_dict(db=db)))
+
+
+@router.get("/order", dependencies=[Depends(auth)])
+async def get_orders(db: Session = Depends(get_db), username: str = Depends(get_current_user)):
+    user = get_user(username, db)
+    orders = get_orders_by_user_id(user.id, db)
+    return JSONResponse(status_code=200, content=jsonable_encoder([order.to_dict(db=db) for order in orders]))
