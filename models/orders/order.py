1cb7f8598091201ff766ceb299f4247973c25b28
marianaandrade <marianaandarde@ua.pt>
2023-12-15 00:41:05 +0000

    change the order

--- models/orders/order.py
+++ models/orders/order.py
@@ -24,6 +24,8 @@ class Order(Base):
     total_price = Column(Float, index=True, default=0, nullable=False)
     total_quantity = Column(Integer, index=True, default=0, nullable=False)
     status = Column(Enum(Status), index=True, default="Accepted", nullable=False)
+    created_at = Column(String(50), default=datetime.datetime.now(), nullable=False)
+    updated_at = Column(String(50), default=datetime.datetime.now(), nullable=False)
     user_id = Column(String(50), ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
 
     def to_dict(self, db: Session = Depends(get_db)):
@@ -33,6 +35,8 @@ class Order(Base):
             "total_price": self.total_price,
             "total_quantity": self.total_quantity,
             "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
             "order_items": [item.to_dict(db=db) for item in get_order_items_by_order_id(self.id, db)],
             "order_history": [item.to_dict(db=db) for item in get_order_history_by_order_id(self.id, db)]
         }
